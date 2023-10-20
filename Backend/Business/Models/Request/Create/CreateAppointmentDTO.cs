998b00642a12f64dd7777efa58a8f89b24629f7e
FURKAN AYDIN <frknydn22@icloud.com>
2023-10-20 15:01:22 +0300

    first commit

---
+++ Backend/Business/Models/Request/Create/CreateAppointmentDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models.Request.Create
+{
+    public class CreateAppointmentDTO
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+        public string Reason { get; set; }
+        public bool IsConfirmed { get; set; }
+        public int UserID { get; set; }
+    }
+}
